f3ed9cb9472769fa5605c2e947cab840382087f6
weikmaks <weikmaks@gmail.com>
2020-06-26 23:58:06 +0300

    v2.01

---
+++ lesson_26/script.js
@@ -0,0 +1,22 @@
+//1
+//2
+//3
+//27.01
+function Cars(brandName, model, productionYear) {
+	this.brandName = brandName;
+	this.model = model;
+	this.productionYear = productionYear;
+}
+let bmw = _.create(Cars.prototype, {
+	brandName: 'BMW',
+	model: 'X7',
+	productionYear: '2020'
+});
+console.log(bmw);
+let invertBmw = _.invert(bmw);
+console.log(invertBmw)
+
+let pickBmw = _.pick(bmw, 'brandName', 'productionYear');
+console.log(pickBmw)
+let omitBmw = _.omit(bmw, 'model');
+console.log(omitBmw)
